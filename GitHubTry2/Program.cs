20f6279af44782441f20d754c710aed30d1ae420
MICHAEL MERCER <u1117450@umail.utah.edu>
2022-09-14 18:01:26 +0900

    This should be the end of this

--- GitHubTry2/Program.cs
+++ GitHubTry2/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//please dont make me fail the lab
+static void FindMaxValue<T>(T[] array)
+{
+    T MaxValue = array[0];
+    foreach(T a in array)
+    {
+        if (a > MaxValue)
+        {
+            MaxValue = a;
+        }
+    }
+    ///here is a comment
+}
